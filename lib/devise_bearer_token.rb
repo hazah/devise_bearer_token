5db37eacc6b2f49726d3dbe38935d91d4dbde945
Ivgeni <ivgenis@gmail.com>
2015-10-01 07:19:30 -0400

    Adds require and moves mod addition before setup

--- lib/devise_bearer_token.rb
+++ lib/devise_bearer_token.rb
@@ -1,9 +1,13 @@
+unless defined?(Devise)
+  require 'devise'  
+end
+
 module DeviseBearerToken
 end
+
+Devise.add_module :bearer_token_authenticatable, model: true, strategy: true
 
 Devise.setup do |config|
   config.bearer_token_fields = [ :access_token, :token_type, :client, :expiry, :uid ]
   config.bearer_token_strategy = :headers
 end
-
-Devise.add_module :bearer_token_authenticatable, model: true, strategy: true
